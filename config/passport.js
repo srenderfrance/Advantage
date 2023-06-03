061eddebc33f9d2389ffe976238109ff5b0c8d39
Saul Renderfrance <srenderfrance@gmail.com>
2023-06-03 21:34:45 +0200

    second test commit

--- config/passport.js
+++ config/passport.js
@@ -10,12 +10,6 @@ module.exports = function (passport) {
         if (!user) {
           return done(null, false, { msg: `${userName} not found.` });
         }
-        if (!user.password) {
-          return done(null, false, {
-            msg:
-              "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
-          });
-        }
         const isMatch = await user.comparePassword(password);
         if (isMatch) {
           return done(null, user);
